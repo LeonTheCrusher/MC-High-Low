72785094a09fe2573f391e08529cb5a742e3b8b1
LeonTheCusher <tristanamore@gmail.com>
2022-06-05 03:07:42 -0500

    Better BTC Copy

--- copy.js
+++ copy.js
@@ -1,5 +1,3 @@
 function copyEvent(id) {
-  var str = document.getElementById(id);
-  window.getSelection().selectAllChildren(str);
-  document.execCommand("Copy");
+  navigator.clipboard.writeText(document.getElementById(id).innerText)
 }
